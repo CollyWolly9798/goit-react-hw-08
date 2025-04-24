77a243f557eb957268cd39e258a0ea83c3bf2835
Oleksandr <collywolly9798@gmail.com>
2025-04-24 12:05:42 +0200

    change ind.scss

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import 'modern-normalize';
-import './index.css';
+import './index.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
